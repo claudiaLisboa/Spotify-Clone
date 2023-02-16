68b24dc3bbac10625a5b6172bc4b8aad02257893
Claudia Lisboa <ccalumby@gmail.com>
2023-02-15 21:25:32 -0500

    fixed loading env variables

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,11 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 /* added new fille shazamCoreApi because Key was exposed */
+/* LOADING ENVIRONMENT VARIBLES IN REACT APP USING VITE
+https://stackoverflow.com/questions/70883903/loading-env-variables-in-react-app-using-vite */
 
+export const API_KEY = import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY;
 export const shazamCoreApi = createApi({ reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set('X-RapidAPI-Key', process.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
+      headers.set('X-RapidAPI-Key', API_KEY);
 
       return headers;
     },
