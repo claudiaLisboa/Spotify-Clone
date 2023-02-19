62ba4bf8b727f3720314acc41b1bdb63287e9d28
Claudia Lisboa <ccalumby@gmail.com>
2023-02-19 18:03:13 -0500

    building topCharts , added hook : useGetTopChartsQuery() /  useSelector() ======== not  fixed yet ERROR in  "GET https://shazam-core.p.rapidapi.com/v1/charts/world 429"

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,14 @@
-const TopCharts = () => <div>TopCharts</div>;
+import React from 'react';
+import { useSelector } from 'react-redux';
 
+import { Error, Loader, SongCard } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
+const TopCharts = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+  const { activeSong, isPlaying } = useSelector((state) => state.player); const TopCharts = () => <div>TopCharts</div>;
+  return (
+    <div />
+  );
+};
 export default TopCharts;
