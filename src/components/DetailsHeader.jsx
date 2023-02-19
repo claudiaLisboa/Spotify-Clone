401436492db62d1b5a998c65ade891caa46550ce
Claudia Lisboa <ccalumby@gmail.com>
2023-02-19 16:14:29 -0500

    added another functionality to img if don't have artistID then render the song data

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom';
-const DetailsHeader = ({ artistId, artistData, songData}) => (
-  <div className="relative w-full flex flex-col">
-     <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-48" />
 
-      <div className="absolute inset-0 flex items-center">
-          <img alt="art"
-               src={artistId?
-               artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}','500').replace('{h}','500')}
-          />
-      </div>
+const DetailsHeader = ({ artistId, artistData, songData }) => (
+  <div className="relative w-full flex flex-col">
+    <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-48" />
+
+    <div className="absolute inset-0 flex items-center">
+      <img
+        alt="art"
+        src={artistId
+          ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500')
+          : songData?.images?.coverart}
+      />
+    </div>
   </div>
 );
 
