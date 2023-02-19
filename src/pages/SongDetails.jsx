1a5c44c20526892c2917650cade5e0ecb30e8f60
Claudia Lisboa <ccalumby@gmail.com>
2023-02-18 21:58:11 -0500

    initialize useDispatch() and useSelector()

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,7 +1,12 @@
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSons } from '../components';
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
 
 const SongDetails = () => {
+  const dispatch = useDispatch();
   const { songid } = useParams();
+  const { activeSong, isPlaying } useSelector((state) => state.player);
 
   console.log(songid);
 
