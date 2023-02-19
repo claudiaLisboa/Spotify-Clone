9a847fd6c1c0b9f245829cd3ad7d23a025a843d3
Claudia Lisboa <ccalumby@gmail.com>
2023-02-19 16:41:39 -0500

    updated functionality

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const DetailsHeader = ({ artistId, artistData, songData }) => (
@@ -7,12 +8,33 @@ const DetailsHeader = ({ artistId, artistData, songData }) => (
     <div className="absolute inset-0 flex items-center">
       <img
         alt="art"
-        src={artistId
-          ? artistData?.artists[artistId].attributes?.artwork?.url.replace('{w}', '500').replace('{h}', '500')
-          : songData?.images?.coverart}
+        src={
+              artistId ? artistData?.artists[artistId].attributes?.artwork?.url
+                .replace('{w}', '500')
+                .replace('{h}', '500')
+                : songData?.images?.coverart
+        }
+        className="sm:w-48 w-28 sm:h-48 h-28 rounded-full
+                object-cover border-2 shadow-xl shadow-black"
       />
+      <div className="ml-5">
+        <p className="font-bold sm:text-3xl text-xl text-white">
+          {artistId ? artistData?.artists[artistId].attributes.name : songData?.title}
+        </p>
+        {!artistId && (
+        <Link to={`/artists/${songData?.artists[0]?.adamid}`}>
+          <p className="text-base text-gray-400 mt-2">{songData?.subtitle}</p>
+        </Link>
+        )}
+
+        <p className="text-base text-gray-400 mt-2">
+          {artistId
+            ? artistData?.attributes?.genreNames[0]
+            : songData?.genres?.primary}
+        </p>
+      </div>
     </div>
+    <div className="w-full sm:h-44 h-24" />
   </div>
 );
-
 export default DetailsHeader;
