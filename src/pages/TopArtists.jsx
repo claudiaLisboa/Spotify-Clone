8d3d1f52214341838d8df7d2ce648145f92d6426
Claudia Lisboa <ccalumby@gmail.com>
2023-02-19 18:44:38 -0500

    import components

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,8 @@
+import React from 'react';
+
+import { ArtistCard, Error, Loader } from '../components';
+import { useGetTopChartsQuery } from '../redux/services/shazamCore';
+
 const TopArtists = () => <div>TopArtists</div>;
 
 export default TopArtists;
