3373ffee83ebd1e9e289087331e032a26ce9697b
Claudia Lisboa <ccalumby@gmail.com>
2023-02-19 18:26:19 -0500

    updated songdetails

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,29 +1,58 @@
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
-  const { songid } = useParams();
+  const { songid, id: artistId } = useParams();
   const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
   const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
-  //console.log(songid);
+  if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
 
+  if (error) return <Error />;
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, index) => {
+    dispatch(setActiveSong({ song, data, index }));
+    dispatch(playPause(true));
+  };
   return (
     <div className="flex flex-col">
-      <DetailsHeader artistId="" songData={songData} />
+      <DetailsHeader
+        artistId="artistId"
+        songData={songData}
+      />
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">AZLyrics : </h2>
-        <div className="mt-5" />
-        {songData?.sections[1].type === 'AZLYRICS'
-          ? songData?.sections[1].text.map((Line, index) => (
-            <p className="text-gray-400 text-base my-1">{Line}</p>
-          )) : <p className="text-gray-400 text-base my-1">Sorry, no azlyrics found!</p> }
+
+        <div className="mt-5">
+          {songData?.sections[1].type === 'AZLYRICS'
+            ? songData?.sections[1]?.text.map((line, index) => (
+              <p key={`lyrics-${line}-${index}`} className="text-gray-400 text-base my-1">{line}</p>
+            ))
+            : (
+              <p className="text-gray-400 text-base my-1">Sorry, no azlyrics found!</p>
+            )}
+        </div>
       </div>
 
+      <RelatedSongs
+        data={data}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
     </div>
   );
 };
