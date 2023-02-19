aaa14a156481da147deb1da8d25c318255274fb8
Claudia Lisboa <ccalumby@gmail.com>
2023-02-18 22:49:46 -0500

    imported  useGetSongDetailsQuery - data: songData

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,18 +1,25 @@
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { DetailsHeader, Error, Loader, RelatedSons } from '../components';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
   const dispatch = useDispatch();
   const { songid } = useParams();
-  const { activeSong, isPlaying } useSelector((state) => state.player);
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
 
   console.log(songid);
 
   return (
-    <div>
-      SongDeatials
+    <div className="flex flex-col">
+      {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">AZLyrics : </h2>
+        <div className="" />
+      </div>
+
     </div>
   );
 };
